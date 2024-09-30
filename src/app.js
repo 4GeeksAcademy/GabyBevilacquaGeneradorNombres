e92d43170c10528cd0d26a97e5c9989c07905152
Gabriella Bevilacqua <bevi.gaby@gmail.com>
2024-09-30 18:04:34 +0000

    practica generador de nombres

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/* eslint-disable 
 import "bootstrap";
 import "./style.css";
 
@@ -7,5 +7,41 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateDomain();
+  console.log("Hello");
+};*/
+const generateDomain = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".net", ".es", ".io"];
+
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let d = 0; d < domain.length; d++) {
+          console.log(pronoun[p] + adj[a] + noun[n] + domain[d]);
+        }
+      }
+    }
+  }
 };
+console.log(generateDomain());
+
+/* 
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let domain = [".com", ".net", ".us", ".io"];
+
+for (let p of pronoun) {                         solucion alternativa mas corta que deberia funcionar
+  for (let a of adj) {                           pero segun la leccion "estandares y lineamientos" hay que evitar las variables globales, es decir,
+    for (let n of noun) {                        que queden fuera de una funcion cuando son variables que solo van a necesitarse dentro de esa 
+      for (let d of domain) {                    funcion en particular.
+        let domainsNames = p + a + n + d;
+        console.log(domainsNames);
+      }
+    }
+  }
+}
+*/
